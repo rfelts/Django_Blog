a5f581e47603f6147d6c695ed03aadacee12b4d3
rfelts <rfelts@adobe.com>
2020-01-28 00:02:36 -0800

    Updating the blogging admin to add categories in the post admin page

--- mysite/blogging/admin.py
+++ mysite/blogging/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
-from blogging.models import Post,Category
+from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+    pass
+
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline,]
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+
